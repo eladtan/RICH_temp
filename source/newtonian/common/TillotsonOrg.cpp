a89a54af3cb559a31c4f4e372f414007cfc70c99
eladtan <eladtan1@gmail.com>
2023-10-10 15:31:29 +0300

    fixed eos

--- source/newtonian/common/TillotsonOrg.cpp
+++ source/newtonian/common/TillotsonOrg.cpp
@@ -10,52 +10,13 @@ TillotsonOrg::TillotsonOrg(double a, double b, double A, double B, double rho0, 
 	a_(a), b_(b), A_(A), B_(B), rho0_(rho0), E0_(E0), EIV_(EIV), ECV_(ECV), alpha_(alpha), beta_(beta),
 	temp_d_(0), temp_p_(0) {}
 
-double TillotsonOrg::dp2EI(double d, double p) const
-{
-	double eta = d / rho0_;
-	double c = E0_ * eta*eta;
-	double AB = (A_ - 2 * B_)*eta + (B_ - A_) + B_ * eta*eta;
-	double A_B = A_ - B_;
-	double sqr = std::sqrt(d*c * 2 * (a_ - b_)*(p - (A_B + eta * B_)*(eta - 1)) + d * d*c*c*(a_ + b_)*(a_ + b_) + (p - (eta - 1)*(A_B + eta * B_))
-		*(p - (eta - 1)*(A_B + eta * B_)));
-	double first_part = p - AB - a_ * c*d - b_ * c*d;
-	double E = (first_part + sqr) / (2 * a_*d);
-	assert(E > 0);
-	double newp = de2pI(d, E);
-	if (newp > 1.00001*p || newp < 0.99999*p)
-		E = p * 1e7 / (a_*d);
-	
-	return E;
-}
-
-double TillotsonOrg::dp2EIV(double d, double p) const
-{
-	double eta = d / rho0_;
-	double mu = eta - 1;
-	double c = E0_ * eta*eta;
-	double A = A_ * mu;
-	double eta2 = alpha_ * (rho0_ / d - 1) * (rho0_ / d - 1);
-	double exp_alpha = (eta2 > 100) ? 0 : std::exp(-eta2);
-	double eta3 = beta_ * (rho0_ / d - 1);
-	double exp_beta = (eta3 > 100) ? 0 : A * std::exp(-eta3);
-	double b = b_ * exp_alpha;
-	double AB = exp_alpha * exp_beta;
-	double E = (p - AB - a_ * c*d - b * c*d + sqrt(4 * a_*c*d*(p - AB) + std::pow(AB + (a_ + b)*c*d - p, 2))) /
-		(2 * a_*d);
-	assert(E > 0);
-	double newp = de2pIV(d, E);
-	if (newp > 1.00001*p || newp < 0.99999*p)
-		E = p * 1e5 / (a_*d);
-	return E;
-}
-
 double TillotsonOrg::de2pI(double d, double e)const
 {
 	double eta = d / rho0_;
 	double c = E0_ * eta*eta;
 	double AB = (A_ - 2 * B_)*eta + (B_ - A_) + B_ * eta*eta;
 	double res = 0;
-	res = std::max((a_ + b_ / (e / c + 1))*d*e + AB, a_*d*e*1e-7);
+	res = std::max((a_ + b_ / (e / c + 1))*d*e + AB, -d * e * 1e-2);
 	return res;
 }
 
@@ -81,21 +42,21 @@ double TillotsonOrg::de2pIV(double d, double e)const
 	return std::max(a_*d*e + exp_alpha * (b_*d*e / (e / c + 1) + exp_beta), a_ *d*e*1e-5);
 }
 
-double TillotsonOrg::dep2cI(double d, double e, double p) const
+double TillotsonOrg::dep2cI(double d, double e) const
 {
 	double eta = d / rho0_;
 	double c = E0_ * eta*eta;
 	double w0 = e / c + 1;
 	double gamma = a_ + b_ / w0;
-	double AB = (A_ - 2 * B_)*eta + (B_ - A_) + B_ * eta*eta;
-	p = (a_ + b_ / (e / c + 1))*d*e + AB;
+	double const p = de2pI(d, e);
 	double res = (gamma + 1)*p / d + (A_ + B_ * (eta*eta - 1)) / d + b_ * (w0 - 1)*(2 * e - p / d) / (w0*w0);
 	res = std::max(res, 1e-10*E0_);
 	return res;
 }
 
-double TillotsonOrg::dep2cIV(double d, double e, double p) const
+double TillotsonOrg::dep2cIV(double d, double e) const
 {
+	double const p = de2pIV(d, e);
 	double eta = d / rho0_;
 	double w0 = e / (E0_*eta*eta) + 1;
 	double z = 1.0 / eta - 1.0;
@@ -108,89 +69,11 @@ double TillotsonOrg::dep2cIV(double d, double e, double p) const
 	return res;
 }
 
-//! \brief Auxiliary struct
-	struct dp2eIIOrg
-	{
-	  /*! \brief Class constructor
-	    \param eos Equation of state
-	   */
-	  explicit dp2eIIOrg(TillotsonOrg const& eos) : eos_(eos)
-		{}
-
-	  /*! \brief Calculate pressure
-	    \param e Energy
-	    \return Pressure
-	   */
-		double operator()(double e)
-		{
-			double res = 1 - eos_.de2pII(eos_.temp_d_, e) / eos_.temp_p_;
-			return res;
-		}
-	private:
-		TillotsonOrg const& eos_;
-	};
-
-
 
 double TillotsonOrg::dp2e(double d, double p, tvector const & /*tracers*/, vector<string> const & /*tracernames*/) const
 {
-	double eta = d / rho0_;
-	double mu = eta - 1;
-	double c = E0_ * eta*eta;
-	double A = A_ * mu;
-	if (d >= rho0_)
-	{
-		return dp2EI(d, p);
-	}
-	else
-	{
-		double e1 = dp2EI(d, p);
-		if (e1<EIV_)
-			return e1;
-		double e4 = dp2EIV(d, p);
-		if (e4 > ECV_)
-			return e4;
-		double PIV = de2pI(d, EIV_);
-		double eta2 = alpha_ * (rho0_ / d - 1) * (rho0_ / d - 1);
-		double exp_alpha = (eta2 > 100) ? 0 : std::exp(-eta2);
-		double eta3 = beta_ * (rho0_ / d - 1);
-		double exp_beta = (eta3 > 100) ? 0 : A * std::exp(-eta3);
-		double PCV = 0;
-		PCV = std::max(a_*d*ECV_ + exp_alpha * (b_*d*ECV_ / (ECV_ / c + 1) + exp_beta), (a_ + b_)*d*ECV_*1e-5);
-		temp_d_ = d;
-		temp_p_ = p;
-		boost::uintmax_t it = 50;
-		std::pair<double, double> res;
-		try
-		{
-			res = boost::math::tools::toms748_solve(dp2eIIOrg(*this), EIV_, ECV_, boost::math::tools::eps_tolerance<double>(30), it);
-		}
-		catch (boost::exception const& /*eo*/)
-		{
-			std::cout << " EIV_ " << EIV_ << " ECV_ " << ECV_ << " density " << d << " pressure " << p << " PIV " << PIV << " PCV " << PCV << std::endl;
-		}
-		double result = 0.5*(res.first + res.second);
-		double newp = de2p(d, result);
-		if (newp > PIV * 2)
-		{
-			if (std::abs(p - newp) > 0.001*std::abs(p))
-			{
-				UniversalError eo("No dp2e convergence");
-				eo.addEntry("Density", d);
-				eo.addEntry("Pressure", p);
-				eo.addEntry("New Pressure", newp);
-				eo.addEntry("EIV", EIV_);
-				eo.addEntry("ECV", ECV_);
-				eo.addEntry("First energy", res.first);
-				eo.addEntry("Second energy", res.second);
-				eo.addEntry("First pressure", de2p(d, res.first));
-				eo.addEntry("Second pressure", de2p(d, res.second));
-				throw eo;
-			}
-		}
-		assert(result > 0);
-		return result;
-	}
+	throw UniversalError("TillotsonOrg::dp2e not implemented");
+	return 0;
 }
 
 double TillotsonOrg::de2p(double d, double e, tvector const& /*tracers*/, vector<string> const& /*tracernames*/) const
@@ -211,31 +94,23 @@ double TillotsonOrg::de2p(double d, double e, tvector const& /*tracers*/, vector
 
 double TillotsonOrg::de2c(double d, double e, tvector const & tracers, vector<string> const & tracernames) const
 {
-	double p = de2p(d, e, tracers, tracernames);
-	return dp2c(d, p, tracers, tracernames);
-}
-
-double TillotsonOrg::dp2c(double d, double p, tvector const & tracers, vector<string> const & tracernames) const
-{
-	double e = dp2e(d, p, tracers, tracernames);
-	if (d >= rho0_  || e<EIV_)
+	if (d >= rho0_  || e < EIV_)
 	{
-		double res = dep2cI(d, e, p);
-		assert(res > 0);
+		double res = dep2cI(d, e);
 		return std::sqrt(res);
 	}
 	else
 	{
 		if (e >= ECV_ )
 		{
-			double res = dep2cIV(d, e, p);
+			double res = dep2cIV(d, e);
 			assert(res > 0);
 			return std::sqrt(res);
 		}
 		else
 		{
-			double c1 = dep2cI(d, e, p);
-			double c4 = dep2cIV(d, e, p);
+			double c1 = dep2cI(d, e);
+			double c4 = dep2cIV(d, e);
 			double res = std::sqrt((c1*(ECV_ - e) + c4 * (e - EIV_)) / (ECV_ - EIV_));
 			assert(res > 0);
 			return res;
@@ -243,17 +118,21 @@ double TillotsonOrg::dp2c(double d, double p, tvector const & tracers, vector<st
 	}
 }
 
+double TillotsonOrg::dp2c(double d, double p, tvector const & tracers, vector<string> const & tracernames) const
+{
+	throw UniversalError("TillotsonOrg::dp2c not implemented");
+	return 0;
+}
+
 double TillotsonOrg::dp2s(double /*d*/, double /*p*/, tvector const & /*tracers*/, vector<string> const & /*tracernames*/) const
 {
-	std::cout << "dp2s not implemented" << std::endl;
-	assert(false);
+	throw UniversalError("TillotsonOrg::dp2s not implemented");
 	return 0;
 }
 
 double TillotsonOrg::sd2p(double /*s*/, double /*d*/, tvector const & /*tracers*/, vector<string> const & /*tracernames*/) const
 {
-	std::cout << "sd2p not implemented" << std::endl;
-	assert(false);
+	throw UniversalError("TillotsonOrg::sd2p not implemented");
 	return 0;
 }
 
