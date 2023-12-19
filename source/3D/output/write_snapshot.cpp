98c6326e0b9743e5115ce4f8f3e287529e304696
Maor Miz <maorm2605@gmail.com>
2023-12-19 10:48:29 +0200

    Added directory creation for HDF5 printing methods

--- source/3D/output/write_snapshot.cpp
+++ source/3D/output/write_snapshot.cpp
@@ -230,6 +230,10 @@ void WriteSnapshot3DHelper(H5File &file, Group &writegroup, Group &tracers, Grou
         std::string myFilePath;
 
         fs::path ranks_files_path = path / fs::path(filename).filename().replace_extension();
+        if(not fs::exists(ranks_files_path))
+        {
+            fs::create_directory(ranks_files_path);
+        }
         MPI_Comm_rank(MPI_COMM_WORLD, &rank);
         MPI_Comm_size(MPI_COMM_WORLD, &ws);
         myFilePath = (ranks_files_path / std::to_string(rank)).string() + ".h5";
