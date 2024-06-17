8a1eb5a607420ee9d01f1db45dcc0d0bf3a8cb42
ItamarShmelo <itamargiron@gmail.com>
2024-06-17 17:39:09 +0300

    removed D_group

--- source/Radiation/MultigroupDiffusion.cpp
+++ source/Radiation/MultigroupDiffusion.cpp
@@ -60,7 +60,6 @@ bool MultigroupDiffusion::prestep(Tessellation3D const& tess) const {
 
     sigma_absorption_group = std::vector<std::vector<double>>(ENERGY_GROUPS_NUM, std::vector<double>(N, 0.0));
     planck_integal_group = std::vector<std::vector<double>>(ENERGY_GROUPS_NUM, std::vector<double>(N, 0.0));
-    D_group = std::vector<std::vector<double>>(ENERGY_GROUPS_NUM, std::vector<double>(N, 0.0));
     R2_group = std::vector<std::vector<double>>(ENERGY_GROUPS_NUM, std::vector<double>(N, 0.0));
     cell_flux_limiter_group = std::vector<std::vector<double>>(ENERGY_GROUPS_NUM, std::vector<double>(N, 0.0));
     
