dd83a585c091f18e0cbca4d1e1a8317324725f10
ItamarShmelo <itamargiron@gmail.com>
2024-06-17 17:42:00 +0300

    decleration and implementation of the base class MultigroupDiffusionBoundaryCalculator and of MultigroupDiffusionSideBoundary which is a MarshakBoundaryCondition

---
+++ source/Radiation/MultigroupDiffusionBoundaryCalculator.cpp
@@ -0,0 +1,100 @@
+#include "MultigroupDiffusionBoundaryCalculator.hpp"
+#include "boost/math/special_functions/pow.hpp"
+#include "RadiationDriver.hpp"
+#include "planck_integral/planck_integral.hpp"
+
+
+MultigroupDiffusionBoundaryCalculator
+::MultigroupDiffusionBoundaryCalculator(std::vector<double> const& energy_groups_center_,
+                                        std::vector<double> const& energy_groups_boundary_) : energy_groups_center(energy_groups_center_),
+                                                                                              energy_groups_boundary(energy_groups_boundary_) {}
+
+MultigroupDiffusionBoundaryCalculator::~MultigroupDiffusionBoundaryCalculator() {}
+
+MultigroupDiffusionSideBoundary
+::MultigroupDiffusionSideBoundary(double const temperature_,
+                                  std::vector<double> const& energy_groups_center_,
+                                  std::vector<double> const& energy_groups_boundary_) : temperature(temperature_),
+                                                                                        Ur(get_radiation_energy_density(temperature_)),
+                                                                                        Ug(ENERGY_GROUPS_NUM, 0.0),
+                                                                                        MultigroupDiffusionBoundaryCalculator(energy_groups_center_, energy_groups_boundary_) {
+
+    for(std::size_t g=0; g<ENERGY_GROUPS_NUM; ++g){
+        Ug[g] = planck_energy_density_group_integral(energy_groups_boundary[g], energy_groups_boundary[g+1], temperature);
+    }
+}
+
+MultigroupDiffusionSideBoundary::~MultigroupDiffusionSideBoundary() {}
+
+void MultigroupDiffusionSideBoundary::setBoundaryValuesGray(Tessellation3D const& tess,
+                                                            std::size_t const index,
+                                                            std::size_t const outside_point,
+                                                            double const dt,
+                                                            std::vector<ComputationalCell3D> const& /*cells*/,
+                                                            double const Area, 
+                                                            double& A,
+                                                            double& b,
+                                                            std::size_t const /*face_index*/) const {
+    
+    using boost::math::pow;
+
+    double const R = tess.GetWidth(index);
+    if(tess.GetMeshPoint(index).x > (tess.GetMeshPoint(outside_point).x + R * 1e-4)){
+        A += 0.5 * CG::speed_of_light * dt * Area;
+        b += 2.0 * Area * dt * Ur;
+    } 
+}
+
+void MultigroupDiffusionSideBoundary::getOutsideValuesGray(Tessellation3D const& tess,
+                                                           std::size_t const index,
+                                                           std::size_t const outside_point,
+                                                           std::vector<ComputationalCell3D> const& cells,
+                                                           std::vector<double> const& new_Er,
+                                                           double& Er_outside,
+                                                           Vector3D& v_outside) const {
+    double const R = tess.GetWidth(index);
+
+    if(tess.GetMeshPoint(index).x > (tess.GetMeshPoint(outside_point).x + R * 1e-4)){
+        Er_outside = Ur;
+    } else {
+        Er_outside = new_Er[index];
+    }
+
+    v_outside = cells[index].velocity;
+}
+
+void MultigroupDiffusionSideBoundary::setBoundaryValuesGroup(std::size_t const group,
+                                                             Tessellation3D const& tess,
+                                                             std::size_t const index, 
+                                                             std::size_t const outside_point,
+                                                             double const dt,
+                                                             std::vector<ComputationalCell3D> const& /*cells*/,
+                                                             double const Area,
+                                                             double& A,
+                                                             double& b,
+                                                             std::size_t const /*face_index*/) const {
+    
+    double const R = tess.GetWidth(index);
+    if(tess.GetMeshPoint(index).x > (tess.GetMeshPoint(outside_point).x + R*1e-4)){
+        A += 0.5 * CG::speed_of_light * dt * Area;
+        b += 2.0 * Area * dt * Ug[group];
+    }
+}
+
+void MultigroupDiffusionSideBoundary::getOutSideValuesGroup(std::size_t const group,
+                                                            Tessellation3D const& tess,
+                                                            std::vector<ComputationalCell3D> const& cells,
+                                                            std::size_t const index,
+                                                            std::size_t const outside_point,
+                                                            std::vector<double> const& new_Eg,
+                                                            double& Eg_outside,
+                                                            Vector3D& /*v_outside*/) const {
+    
+    double const R = tess.GetWidth(index);
+    if(tess.GetMeshPoint(index).x > (tess.GetMeshPoint(outside_point).x + R*1e-4)){
+        Eg_outside = Ug[group];
+    } else {
+        Eg_outside = new_Eg[group];
+    }
+}
+
---
+++ source/Radiation/MultigroupDiffusionBoundaryCalculator.hpp
@@ -0,0 +1,109 @@
+#ifndef MULTI_GROUP_DIFFUSION_BOUNDARY_CALCULATOR_HPP
+#define MULTI_GROUP_DIFFUSION_BOUNDARY_CALCULATOR_HPP
+
+#include "conj_grad_solve.hpp"
+
+class MultigroupDiffusionBoundaryCalculator {
+public:
+    MultigroupDiffusionBoundaryCalculator(std::vector<double> const& energy_groups_center_,
+                                          std::vector<double> const& energy_groups_boundary_);
+
+    virtual ~MultigroupDiffusionBoundaryCalculator();
+
+    virtual void setBoundaryValuesGroup(std::size_t const group,
+                                        Tessellation3D const& tess,
+                                        std::size_t const index, 
+                                        std::size_t const outside_point,
+                                        double const dt,
+                                        std::vector<ComputationalCell3D> const& cells,
+                                        double const Area,
+                                        double& A,
+                                        double& b,
+                                        std::size_t const face_index) const = 0;
+    
+    virtual void getOutSideValuesGroup(std::size_t const group,
+                                       Tessellation3D const& tess,
+                                       std::vector<ComputationalCell3D> const& cells,
+                                       std::size_t const index,
+                                       std::size_t const outside_point,
+                                       std::vector<double> const& new_Eg,
+                                       double& Eg_outside,
+                                       Vector3D& v_outside) const = 0;
+    
+    virtual void setBoundaryValuesGray(Tessellation3D const& tess,
+                                       std::size_t const index,
+                                       std::size_t const outside_point,
+                                       double const dt,
+                                       std::vector<ComputationalCell3D> const& cells,
+                                       double const Area, 
+                                       double& A,
+                                       double& b,
+                                       std::size_t const face_index) const = 0;
+
+    virtual void getOutsideValuesGray(Tessellation3D const& tess,
+                                      std::size_t const index,
+                                      std::size_t const outside_point,
+                                      std::vector<ComputationalCell3D> const& cells,
+                                      std::vector<double> const& new_Er,
+                                      double& Er_outside,
+                                      Vector3D& v_outside) const = 0;
+
+    std::vector<double> const energy_groups_center;
+    std::vector<double> const energy_groups_boundary;
+
+};
+
+class MultigroupDiffusionSideBoundary : public MultigroupDiffusionBoundaryCalculator {
+public:
+
+    MultigroupDiffusionSideBoundary(double const temperature_,
+                                    std::vector<double> const& energy_group_center_,
+                                    std::vector<double> const& energy_group_boundary_);
+
+    ~MultigroupDiffusionSideBoundary();
+
+    void setBoundaryValuesGroup(std::size_t const group,
+                                Tessellation3D const& tess,
+                                std::size_t const index, 
+                                std::size_t const outside_point,
+                                double const dt,
+                                std::vector<ComputationalCell3D> const& cells,
+                                double const Area,
+                                double& A,
+                                double& b,
+                                std::size_t const face_index) const override;
+    
+    void getOutSideValuesGroup(std::size_t const group,
+                               Tessellation3D const& tess,
+                               std::vector<ComputationalCell3D> const& cells,
+                               std::size_t const index,
+                               std::size_t const outside_point,
+                               std::vector<double> const& new_Eg,
+                               double& Eg_outside,
+                               Vector3D& v_outside) const override;
+    
+    void setBoundaryValuesGray(Tessellation3D const& tess,
+                               std::size_t const index,
+                               std::size_t const outside_point,
+                               double const dt,
+                               std::vector<ComputationalCell3D> const& cells,
+                               double const Area, 
+                               double& A,
+                               double& b,
+                               std::size_t const face_index) const override;
+
+    void getOutsideValuesGray(Tessellation3D const& tess,
+                              std::size_t const index,
+                              std::size_t const outside_point,
+                              std::vector<ComputationalCell3D> const& cells,
+                              std::vector<double> const& new_Er,
+                              double& Er_outside,
+                              Vector3D& v_outside) const override;
+
+    private:
+        double const temperature;
+        double const Ur;
+        mutable std::vector<double> Ug;
+};
+
+#endif // MULTI_GROUP_DIFFUSION_BOUNDARY_CALCULATOR_HPP
