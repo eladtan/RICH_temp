0b457c03eadb4e83771c41d5eda012637f93863d
ItamarShmelo <itamargiron@gmail.com>
2024-07-04 14:18:46 +0300

    added a MultigroupDiffusionXInflowBoundary boundary condition

--- source/Radiation/MultigroupDiffusionBoundaryCalculator.cpp
+++ source/Radiation/MultigroupDiffusionBoundaryCalculator.cpp
@@ -2,6 +2,7 @@
 #include "boost/math/special_functions/pow.hpp"
 #include "RadiationDriver.hpp"
 #include "planck_integral/planck_integral.hpp"
+#include "Diffusion.hpp" // for CalcSingleFluxLimiter
 
 
 MultigroupDiffusionBoundaryCalculator
@@ -132,4 +133,255 @@ void MultigroupDiffusionSideBoundary::setMomentumTermBoundaryGroup(std::size_t c
     } else {
         A += 2.0 * dt * momentum_term_coefficient;
     }
+}
+
+MultigroupDiffusionXInflowBoundary::MultigroupDiffusionXInflowBoundary(std::vector<double> const& energy_groups_center_,
+                                                                       std::vector<double> const& energy_groups_boundary_,
+                                                                       ComputationalCell3D const& left_state,
+                                                                       ComputationalCell3D const& right_state,
+                                                                       MultigroupDiffusionCoefficientCalculator const& coefficient_calculator) : left_state_(left_state),
+                                                                                                 right_state_(right_state),
+                                                                                                 coefficient_calculator_(coefficient_calculator),
+                                                                                                 MultigroupDiffusionBoundaryCalculator(energy_groups_center_, energy_groups_boundary_) {}
+
+void MultigroupDiffusionXInflowBoundary::setBoundaryValuesGroup(std::size_t const group,
+                                                                Tessellation3D const& tess,
+                                                                std::size_t const index, 
+                                                                std::size_t const outside_point,
+                                                                double const dt,
+                                                                std::vector<ComputationalCell3D> const& cells,
+                                                                double const Area,
+                                                                double& A,
+                                                                double& b,
+                                                                std::size_t const face_index) const {
+    double const R = tess.GetWidth(index);
+    if(tess.GetMeshPoint(index).x < (tess.GetMeshPoint(outside_point).x  + R*1e-4)){
+        double const Eg_j = left_state_.Eg[group] * left_state_.density;
+        double const Eg_i = cells[index].Eg[group] * cells[index].density;
+
+        double const dx = tess.GetCellCM(index).x - tess.GetBoxCoordinates().first.x;
+        Vector3D const gradient = Vector3D(1.0, 0.0, 0.0) * (1.0 / (2.0 * dx));
+        Vector3D const r_ij = tess.GetMeshPoint(index) - tess.GetMeshPoint(outside_point);
+
+        // max temperature?
+        double const Dg_i = coefficient_calculator_.CalcDiffusionCoefficientGroup(cells[index], group);
+        double const Dg_j = coefficient_calculator_.CalcDiffusionCoefficientGroup(cells[outside_point], group);
+        
+        // this is supposed to be harmonic probably but for the sake of sake
+        double const Dg_ij = 0.5 * (Dg_i + Dg_j);
+        
+        // what if there is no flux limiter?    
+        double const lambda_g_ij = CG::CalcSingleFluxLimiter(gradient * (Eg_i - Eg_j), Dg_ij, 0.5*(Eg_i + Eg_j));
+
+        double const lambda_gDg_ij = lambda_g_ij * Dg_ij;
+
+        // WARNING: This only works if the length scale is 1.0 since tess.GetArea and r_ij are not to scaled! 
+        double const flux = ScalarProd(gradient, r_ij * (tess.GetArea(face_index) / abs(r_ij))) * dt * lambda_gDg_ij;
+        
+        A += flux;
+        b += flux * Eg_j;
+    } else if (tess.GetMeshPoint(index).x > (tess.GetMeshPoint(outside_point).x - R*1e-4)){
+        double const Eg_j = right_state_.Eg[group] * right_state_.density;
+        double const Eg_i = cells[index].Eg[group] * cells[index].density;
+
+        
+        double const dx = tess.GetBoxCoordinates().second.x - tess.GetCellCM(index).x;
+        Vector3D const gradient = Vector3D(-1.0, 0.0, 0.0) * (1.0 / (2.0 * dx));
+        Vector3D const r_ij = tess.GetMeshPoint(index) - tess.GetMeshPoint(outside_point);
+        
+        // max temperature?
+        double const Dg_i = coefficient_calculator_.CalcDiffusionCoefficientGroup(cells[index], group);
+        double const Dg_j = coefficient_calculator_.CalcDiffusionCoefficientGroup(cells[outside_point], group);
+        
+        // this is supposed to be harmonic probably but for the sake of sake
+        double Dg_ij = 0.5 * (Dg_i + Dg_j);
+
+        // what if there is no flux limiter?    
+        double const lambda_g_ij = CG::CalcSingleFluxLimiter(gradient * (Eg_i - Eg_j), Dg_ij, 0.5*(Eg_i + Eg_j));
+        double const lambda_gDg_ij = lambda_g_ij * Dg_ij;
+        
+        // WARNING: This only works if the length scale is 1.0 since tess.GetArea and r_ij are not to scaled! 
+        double const flux = ScalarProd(gradient, r_ij * (tess.GetArea(face_index) / abs(r_ij))) * dt * lambda_gDg_ij;
+        
+        A += flux;
+        b += flux * Eg_j;
+    }
+}
+
+void MultigroupDiffusionXInflowBoundary::getOutsideValuesGroup(std::size_t const group,
+                                                               Tessellation3D const& tess,
+                                                               std::size_t const index,
+                                                               std::size_t const outside_point,
+                                                               std::vector<ComputationalCell3D> const& cells,
+                                                               std::vector<double> const& new_Eg,
+                                                               double& Eg_outside,
+                                                               Vector3D& v_outside) const {
+    double const R = tess.GetWidth(index);
+
+    if(tess.GetMeshPoint(index).x > (tess.GetMeshPoint(outside_point).x + R * 1e-4)){
+        Eg_outside = left_state_.Eg[group] * left_state_.density;
+        v_outside  = left_state_.velocity;
+    } else if (tess.GetMeshPoint(index).x < (tess.GetMeshPoint(outside_point).x - R * 1e-4)){
+        Eg_outside = right_state_.Eg[group] * right_state_.density;
+        v_outside  = right_state_.velocity;
+    } else {
+        Eg_outside = new_Eg[index];
+        Vector3D const normal = normalize(tess.GetMeshPoint(outside_point) - tess.GetMeshPoint(index));
+
+        v_outside = cells[index].velocity;
+        v_outside -=  normal * (2.0 * ScalarProd(normal, v_outside));
+    }       
+}
+
+void MultigroupDiffusionXInflowBoundary::setBoundaryValuesGray(Tessellation3D const& tess,
+                                                               std::size_t const index,
+                                                               std::size_t const outside_point,
+                                                               double const dt,
+                                                               std::vector<ComputationalCell3D> const& cells,
+                                                               double const Area, 
+                                                               double& A,
+                                                               double& b,
+                                                               std::size_t const face_index) const {
+    double const R = tess.GetWidth(index);
+
+    if(tess.GetMeshPoint(index).x < (tess.GetMeshPoint(outside_point).x + R*1e-4)){
+        double const dx = tess.GetCellCM(index).x - tess.GetBoxCoordinates().first.x;
+        Vector3D const gradient = Vector3D(1.0, 0.0, 0.0) * (1.0 / (2.0 * dx));
+        Vector3D const r_ij = tess.GetMeshPoint(index) - tess.GetMeshPoint(outside_point);
+
+        double lambdaD_i = 0.0;
+        double lambdaD_j = 0.0;
+        double sum_Eg_i = 0.0;
+        double sum_Eg_j = 0.0;
+        for(std::size_t g = 0; g < ENERGY_GROUPS_NUM; ++g){
+            double const Eg_j = left_state_.Eg[g] * left_state_.density;
+            double const Eg_i = cells[index].Eg[g] * cells[index].density;
+
+            double const Dg_i = coefficient_calculator_.CalcDiffusionCoefficientGroup(cells[index], g);
+            double const Dg_j = coefficient_calculator_.CalcDiffusionCoefficientGroup(cells[outside_point], g);
+
+            double Dg_ij = 0.5 * (Dg_i + Dg_j);
+            double const lambda_g_ij = CG::CalcSingleFluxLimiter(gradient * (Eg_i - Eg_j), Dg_ij, 0.5*(Eg_i + Eg_j));
+
+            double const  lambda_gDg_ij = lambda_g_ij * Dg_ij;
+
+            lambdaD_i += lambda_gDg_ij * Eg_i;
+            lambdaD_j += lambda_gDg_ij * Eg_j;
+
+            sum_Eg_i += Eg_i;
+            sum_Eg_j += Eg_j;
+        }
+
+        lambdaD_i /= sum_Eg_i;
+        lambdaD_i /= sum_Eg_j;
+
+        double const flux_coefficient = ScalarProd(gradient, r_ij * (tess.GetArea(face_index) / abs(r_ij))) * dt;
+        
+        double const flux_i_to_j = flux_coefficient * lambdaD_i;
+        double const flux_j_to_i = flux_coefficient * lambdaD_j;
+
+        A += flux_i_to_j;
+        b += flux_j_to_i * left_state_.Erad;
+
+    } else if (tess.GetMeshPoint(index).x > (tess.GetMeshPoint(outside_point).x - R*1e-4)){
+        double const dx = tess.GetBoxCoordinates().second.x - tess.GetCellCM(index).x;
+        Vector3D const gradient = Vector3D(-1.0, 0.0, 0.0) * (1.0 / (2.0 * dx));
+        Vector3D const r_ij = tess.GetMeshPoint(index) - tess.GetMeshPoint(outside_point);
+
+        double lambdaD_i = 0.0;
+        double lambdaD_j = 0.0;
+        double sum_Eg_i = 0.0;
+        double sum_Eg_j = 0.0;
+
+        for(std::size_t g=0; g < ENERGY_GROUPS_NUM; ++g){
+            double const Eg_j = right_state_.Eg[g] * right_state_.density;
+            double const Eg_i = cells[index].Eg[g] * cells[index].density;
+
+            double const Dg_i = coefficient_calculator_.CalcDiffusionCoefficientGroup(cells[index], g);
+            double const Dg_j = coefficient_calculator_.CalcDiffusionCoefficientGroup(cells[outside_point], g);
+
+            double Dg_ij = 0.5 * (Dg_i + Dg_j);
+            double const lambda_g_ij = CG::CalcSingleFluxLimiter(gradient * (Eg_i - Eg_j), Dg_ij, 0.5*(Eg_i + Eg_j));
+
+            double const  lambda_gDg_ij = lambda_g_ij * Dg_ij;
+
+            lambdaD_i += lambda_gDg_ij * Eg_i;
+            lambdaD_j += lambda_gDg_ij * Eg_j;
+
+            sum_Eg_i += Eg_i;
+            sum_Eg_j += Eg_j;
+        }
+        
+
+        lambdaD_i /= sum_Eg_i;
+        lambdaD_j /= sum_Eg_j;
+
+        double const flux_coefficient = ScalarProd(gradient, r_ij * (tess.GetArea(face_index) / abs(r_ij))) * dt;
+
+        double const flux_i_to_j = flux_coefficient * lambdaD_i;
+        double const flux_j_to_i = flux_coefficient * lambdaD_j;
+
+        A += flux_i_to_j;
+        b += flux_j_to_i * right_state_.Erad;
+    }
+}
+
+void MultigroupDiffusionXInflowBoundary::getOutsideValuesGray(Tessellation3D const& tess,
+                                                              std::size_t const index,
+                                                              std::size_t const outside_point,
+                                                              std::vector<ComputationalCell3D> const& cells,
+                                                              std::vector<double> const& new_Er,
+                                                              double& Er_outside,
+                                                              Vector3D& v_outside) const { 
+    double const R = tess.GetWidth(index);
+
+    if(tess.GetMeshPoint(index).x > (tess.GetMeshPoint(outside_point).x + R * 1e-4)){
+        Er_outside = left_state_.Erad;
+        v_outside  = left_state_.velocity;
+    } else if(tess.GetMeshPoint(index).x < (tess.GetMeshPoint(outside_point).x - R * 1e-4)) {
+        Er_outside = right_state_.Erad;
+        v_outside  = right_state_.velocity;
+    } else {
+        Er_outside = new_Er[index];
+
+        Vector3D const normal = normalize(tess.GetMeshPoint(outside_point) - tess.GetMeshPoint(index));
+
+        v_outside = cells[index].velocity;
+        v_outside -=  normal * (2.0 * ScalarProd(normal, v_outside));
+    }
+}
+
+void MultigroupDiffusionXInflowBoundary::setMomentumTermBoundaryGroup(std::size_t const group,
+                                                                      Tessellation3D const& tess,
+                                                                      std::size_t const index,
+                                                                      std::size_t const outside_point,
+                                                                      double dt,
+                                                                      std::vector<ComputationalCell3D> const& cells,
+                                                                      double const momentum_term_coefficient,
+                                                                      double& A,
+                                                                      double& b) const {
+
+    double const R = tess.GetWidth(index);
+
+    if(tess.GetMeshPoint(index).x < (tess.GetMeshPoint(outside_point).x + R * 1e-4)){
+        A += momentum_term_coefficient;
+        b -= momentum_term_coefficient * left_state_.Eg[group] * left_state_.density;
+    } else if(tess.GetMeshPoint(index).x > (tess.GetMeshPoint(outside_point).x - R * 1e-4)){
+        A += momentum_term_coefficient;
+        A -= momentum_term_coefficient * right_state_.Eg[group] * left_state_.density;
+    } else {
+        A += 2.0 * momentum_term_coefficient;
+    }
+}
+
+void MultigroupDiffusionXInflowBoundary::setMomentumTermBoundaryGray(Tessellation3D const& tess,
+                                                                     std::size_t const index,
+                                                                     std::size_t const outside_point,
+                                                                     double dt,
+                                                                     std::vector<ComputationalCell3D> const& cells,
+                                                                     double const momentum_term_coefficient,
+                                                                     double& A,
+                                                                     double& b) const {
+    // this is not true since the momentum term needs to also be weight by the energy density of the groups We should use the energy!
+
 }
--- source/Radiation/MultigroupDiffusionBoundaryCalculator.hpp
+++ source/Radiation/MultigroupDiffusionBoundaryCalculator.hpp
@@ -2,6 +2,7 @@
 #define MULTI_GROUP_DIFFUSION_BOUNDARY_CALCULATOR_HPP
 
 #include "conj_grad_solve.hpp"
+#include "MultigroupDiffusionCoefficientCalculator.hpp"
 
 class MultigroupDiffusionBoundaryCalculator {
 public:
@@ -142,6 +143,76 @@ public:
         double const temperature;
         double const Ur;
         mutable std::vector<double> Ug;
+};
+
+
+class MultigroupDiffusionXInflowBoundary : public MultigroupDiffusionBoundaryCalculator {
+    public:
+        MultigroupDiffusionXInflowBoundary(std::vector<double> const& energy_group_center_,
+                                           std::vector<double> const& energy_group_boundary_,
+                                           ComputationalCell3D const& left_state,
+                                           ComputationalCell3D const& right_state,
+                                           MultigroupDiffusionCoefficientCalculator const& coefficient_calculator);
+
+        void setBoundaryValuesGroup(std::size_t const group,
+                                    Tessellation3D const& tess,
+                                    std::size_t const index, 
+                                    std::size_t const outside_point,
+                                    double const dt,
+                                    std::vector<ComputationalCell3D> const& cells,
+                                    double const Area,
+                                    double& A,
+                                    double& b,
+                                    std::size_t const face_index) const override;
+        
+        void getOutsideValuesGroup(std::size_t const group,
+                                Tessellation3D const& tess,
+                                std::size_t const index,
+                                std::size_t const outside_point,
+                                std::vector<ComputationalCell3D> const& cells,
+                                std::vector<double> const& new_Eg,
+                                double& Eg_outside,
+                                Vector3D& v_outside) const override;
+        
+        void setMomentumTermBoundaryGroup(std::size_t const group,
+                                        Tessellation3D const& tess,
+                                        std::size_t const index,
+                                        std::size_t const outside_point,
+                                        double dt,
+                                        std::vector<ComputationalCell3D> const& cells,
+                                        double const momentum_term_coefficient,
+                                        double& A,
+                                        double& b) const override;
+        
+        void setBoundaryValuesGray(Tessellation3D const& tess,
+                                std::size_t const index,
+                                std::size_t const outside_point,
+                                double const dt,
+                                std::vector<ComputationalCell3D> const& cells,
+                                double const Area, 
+                                double& A,
+                                double& b,
+                                std::size_t const face_index) const override;
+
+        void getOutsideValuesGray(Tessellation3D const& tess,
+                                std::size_t const index,
+                                std::size_t const outside_point,
+                                std::vector<ComputationalCell3D> const& cells,
+                                std::vector<double> const& new_Er,
+                                double& Er_outside,
+                                Vector3D& v_outside) const override;
+        
+        void setMomentumTermBoundaryGray(Tessellation3D const& tess,
+                                        std::size_t const index,
+                                        std::size_t const outside_point,
+                                        double dt,
+                                        std::vector<ComputationalCell3D> const& cells,
+                                        double const momentum_term_coefficient,
+                                        double& A,
+                                        double& b) const override;
+    private:
+        ComputationalCell3D const& left_state_, right_state_;
+        MultigroupDiffusionCoefficientCalculator const& coefficient_calculator_;
 };
 
 #endif // MULTI_GROUP_DIFFUSION_BOUNDARY_CALCULATOR_HPP
