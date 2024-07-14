b12d392454bff86737fa276209c1a63c0a26e8fa
eladtan <eladtan1@gmail.com>
2024-07-14 12:26:48 +0300

    added protections agaisngt floating point underflow

--- source/Radiation/conj_grad_solve.cpp
+++ source/Radiation/conj_grad_solve.cpp
@@ -1,6 +1,10 @@
 #include "conj_grad_solve.hpp"
 #include <limits>
 #include <vectorclass.h>
+#include "boost/math/special_functions/pow.hpp"
+
+using boost::math::pow;
+
 namespace CG
 {
     // Matrix times vector
@@ -409,6 +413,7 @@ namespace CG
         // this loop must be serial b/c CG is an iterative method
 
         sub_p = sub_r;
+        double max_sub_x = 0;
 
         for (int i = 0; i < max_iter; i++) {
             // note: make sure matrix is big enough for the number of processors you are using!
@@ -416,14 +421,20 @@ namespace CG
             max_data[0].mpi_id = rank;
             // r_old = sub_r;                 // Store previous residual
             sub_r_sqrd_old = sub_r_sqrd;  // save a recalculation of r_old^2 later
-
+            if(std::min(sub_r_sqrd_old, std::abs(rho0)) < std::numeric_limits<double>::min()*1e100){
+                good_end = true;
+                sub_x_solution = sub_x;
+                sub_x_solution.resize(Nlocal);
+                break;
+            }
             y = vector_rescale(sub_p, M);
 #ifdef RICH_MPI
             MPI_exchange_data2(tess, y, true);
 #endif
             mat_times_vec(A, A_indeces, y, v);
             y.resize(Nlocal);
-            double const alpha = rho0 / mpi_dot_product(sub_r0, v);
+            double const sub_r0_v = mpi_dot_product(sub_r0, v);
+            double const alpha = std::abs(sub_r0_v) < std::numeric_limits<double>::min()*1e100 ? 0.0 : rho0 / sub_r0_v;
             vec_lin_combo(1.0, sub_x, alpha, y, h);
             vec_lin_combo(1.0, sub_r, -alpha, v, s);
             z = vector_rescale(s, M);
@@ -435,6 +446,7 @@ namespace CG
             double const up = mpi_dot_product(vector_rescale(t, M), vector_rescale(s, M));
             double const down = mpi_dot_product(vector_rescale(t, M), vector_rescale(t, M));
             double const w = up / down;
+            max_sub_x = 0;
             old_x = sub_x;
             vec_lin_combo(1.0, h, w, z, sub_x);
             vec_lin_combo(1.0, s, -w, t, sub_r);
@@ -446,6 +458,11 @@ namespace CG
                 max_data[0].val = 0;
                 max_data[1].val = 0;
                 max_data[2].val = 0;
+                for(size_t j = 0; j < Nlocal; ++j)
+                    max_sub_x = std::max(max_sub_x, std::abs(sub_x[j]));
+#ifdef RICH_MPI
+                MPI_Allreduce(MPI_IN_PLACE, &max_sub_x, 1, MPI_DOUBLE, MPI_MAX, MPI_COMM_WORLD);
+#endif
                 for(size_t j = 0; j < Nlocal; ++j)
                 {
                     double const local_scale = std::abs(b[j]);
@@ -459,7 +476,7 @@ namespace CG
                         max_data[0].val = std::abs(sub_x[j] - old_x[j]) / (std::abs(sub_x[j]) + std::numeric_limits<double>::min() * 100 + maxA[0] * 1e-9);
                         max_loc0 = j;
                     }
-                    if(sub_x[j] < 0)
+                    if(sub_x[j] < -max_sub_x * 1e-20)
                     {
                         max_loc2 = j;
                         max_data[2].val = 1;
@@ -488,8 +505,10 @@ namespace CG
                     sub_x_solution.resize(Nlocal);
                     for(size_t k = 0; k < Nlocal; ++k)
                     {
-                        sub_x_solution[k] = sub_x[k] + sub_r[k] / (tess.GetVolume(k) * 7e10 * 7e10 * 7e10);
-                        if(sub_x_solution[k] < sub_x[k] * 0.5)
+                        sub_x_solution[k] = sub_x[k] + sub_r[k] / (tess.GetVolume(k) * pow<3>(matrix_builder.GetLengthScale()));
+                        if(sub_x_solution[k] < 0)
+                            sub_x_solution[k] = sub_x[k];
+                        if(sub_x_solution[k] < sub_x[k] * 0.5 && sub_x_solution[k] > 0)
                             sub_x_solution[k] = sub_x[k] * 0.5;
                         if(sub_x_solution[k] > sub_x[k] * 2)
                             sub_x_solution[k] = sub_x[k] * 2;
