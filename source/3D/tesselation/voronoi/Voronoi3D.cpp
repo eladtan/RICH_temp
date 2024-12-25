de65937dfa8ceb3936b8b746c72f3b487e56be8b
Maor Miz <maorm2605@gmail.com>
2024-12-25 16:31:35 +0200

    Now prints amount of new point brought in Voronoi build

--- source/3D/tesselation/voronoi/Voronoi3D.cpp
+++ source/3D/tesselation/voronoi/Voronoi3D.cpp
@@ -1853,6 +1853,7 @@ Voronoi3D::DetermineNextIterationPoints(size_t iterations,
         selfIgnorePoints.insert(indices.second);
     }
 
+    size_t total_new_points = 0;
     while(true) // loop is not really infinite (has 'break')
     {
         boost::container::flat_map<size_t, size_t> numOfResultsForSmallPoints;
@@ -1906,6 +1907,14 @@ Voronoi3D::DetermineNextIterationPoints(size_t iterations,
         this->tetra_centers_.resize(this->R_.size());
         this->bigtet_ = SetPointTetras(this->PointTetras_, this->Norg_, this->del_.tetras_, this->del_.empty_tetras_);
         
+        
+        size_t new_points_until_now = std::accumulate(this->Nghost_.cbegin(), this->Nghost_.cend(), 0, [](const size_t &a, const std::vector<size_t> &b){return a + b.size();});
+        size_t new_points = new_points_until_now - total_new_points;
+        total_new_points = new_points_until_now;
+        MPI_Allreduce(MPI_IN_PLACE, &new_points, 1, MPI_UNSIGNED_LONG_LONG, MPI_SUM, comm);
+
+        if(rank == 0) std::cout << "added new points: " << new_points << std::endl;
+
         std::tie(smallPoints, largePoints) = this->DetermineNextIterationPoints(iterations, firstLargeIteration, currentRadiuses, numOfResultsForSmallPoints, numOfResultsForBigPoints);
 
         // #ifdef RICH_MPI
