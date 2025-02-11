31005b4ab9a4b9d80768b27df7f42cec2d49c8a5
eladtan <eladtan1@gmail.com>
2025-02-11 15:21:30 +0200

    fixed paths

--- compilation/SystemLibsLinks.py
+++ compilation/SystemLibsLinks.py
@@ -6,8 +6,8 @@
     "intel_hdf5_lib_dir": "/software/x86_64/5.14.0/hdf5/1.14.3/Intel/OneApi/2024.2.1/enable-cxx/lib",
 
     "vtk"         : "/software/x86_64/5.14.0/vtk/9.3.0/gcc/12.3.0/with_mesa/",
-    
-    'vtk_intel':    "/software/x86_64/5.14.0/vtk/9.3.0/Intel/OneApi/2024.2.1/with_mesa",
+    "vtk_intel"         : "/software/x86_64/5.14.0/vtk/9.3.0/gcc/12.3.0/with_mesa/",
+    # 'vtk_intel':    "/software/x86_64/5.14.0/vtk/9.3.0/Intel/OneApi/2024.2.1/with_mesa",
 
     'ifort'    : "/software/x86_64/5.14.0/intel/OneApi/2024.2.1/compiler/2024.2/bin/ifx",
     'icx-cc'      : "/software/x86_64/5.14.0/intel/OneApi/2024.2.1/compiler/2024.2/bin/icx-cc",
@@ -23,8 +23,10 @@
     'mpicc_gcc'    : "/software/x86_64/5.14.0/openmpi/4.1.6_with_ucc/gcc/12.3.0/bin/mpicc",
     'mpic++_gcc'   : "/software/x86_64/5.14.0/openmpi/4.1.6_with_ucc/gcc/12.3.0/bin/mpic++",
 
-    'mpicc_intel'    : "/software/x86_64/5.14.0/intel/OneApi/2024.2.1/mpi/latest/bin/mpicc",
-    'mpic++_intel'   : "/software/x86_64/5.14.0/intel/OneApi/2024.2.1/mpi/latest/bin/mpicxx",
+    # 'mpicc_intel'    : "/software/x86_64/5.14.0/intel/OneApi/2024.2.1/mpi/latest/bin/mpiicx",
+    # 'mpic++_intel'   : "/software/x86_64/5.14.0/intel/OneApi/2024.2.1/mpi/latest/bin/mpiicpx",
+    'mpicc_intel'    : "/software/x86_64/5.14.0/openmpi/4.1.6/Intel/OneApi/2024.2.1/bin/mpicc",
+    'mpic++_intel'   : "/software/x86_64/5.14.0/openmpi/4.1.6/Intel/OneApi/2024.2.1/bin/mpicxx",
 
 #    'pybind11'   : "/software/x86_64/5.14.0-284.11.1.el9_2.x86_64/pybind11/2.6.1/mock_install/share/cmake/pybind11",
 
