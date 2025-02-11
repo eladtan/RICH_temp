ec50f82a7aab622828d541cfacdccacb3d4f7997
eladtan <eladtan1@gmail.com>
2025-02-11 15:21:18 +0200

    removed uneeded flag

--- compilation/_build.py
+++ compilation/_build.py
@@ -18,7 +18,7 @@ RELEASE_OPTIMIZATION_LEVEL = "-O3"
 
 def _run_cmake(*, build_dir, exe_name, config, SysLibsDict, test_dir, args=None, definitionOfReal=8):
     warning_flags = " -Wextra -Wshadow -Wunused-value -Wunused-variable -Wunused-function -Wunused-macros"
-    common_cxx_flags = f" -std=c++17 {warning_flags} -fno-common -fstack-protector-all -rdynamic -g -DENERGY_GROUPS_NUM={int(args.energy_groups_num)} "
+    common_cxx_flags = f" {warning_flags} -fno-common -fstack-protector-all -rdynamic -g -DENERGY_GROUPS_NUM={int(args.energy_groups_num)} "
     common_cxx_flags_debug = " -DDEBUG -O0 -g3 -gdwarf-3 "
     common_cxx_flags_release = f" -DNDEBUG -DOMPI_SKIP_MPICXX {RELEASE_OPTIMIZATION_LEVEL}"
     
