f4ca9107316c8765972a8493c1f4e83b5f38c7e6
eladtan <eladtan1@gmail.com>
2025-02-11 15:30:58 +0200

    updated time step criteria

--- source/Radiation/MultigroupDiffusion.cpp
+++ source/Radiation/MultigroupDiffusion.cpp
@@ -241,14 +241,14 @@ double MultigroupDiffusion::calculate_dt(double const dt,
 				to_calc = false;
 		if(not to_calc)
 			continue;
-		double const equlibrium_factor = std::abs(cells[i].temperature - std::pow(new_Er[i] / CG::radiation_constant, 0.25)) < 0.05 * cells[i].temperature ? 0.25 : 1;
+		double const equlibrium_factor = std::abs(cells[i].temperature - std::pow(new_Er[i] / CG::radiation_constant, 0.25)) < 0.05 * cells[i].temperature ? 0.5 : 1;
 		double diff = equlibrium_factor * std::abs(cells[i].Erad * cells[i].density - old_Er[i]) / (cells[i].Erad * cells[i].density+0.02*max_Er);
         bool const Erad_equib = std::abs(cells[i].Erad * cells[i].density - old_Er[i]) < 0.075 * old_Er[i];
-        double temp_diff = cells[i].density * equlibrium_factor* std::abs(cells[i].temperature - old_Tm[i]) / (cells[i].density * cells[i].temperature+1e-4*max_rhoT);
+        double temp_diff = cells[i].density * equlibrium_factor* std::abs(cells[i].temperature - old_Tm[i]) / (cells[i].density * cells[i].temperature+1e-3*max_rhoT);
         if(Erad_equib)
             temp_diff *= 0.25;
-        if(fleck_factor[i] < 0.5)
-            temp_diff *= 0.5;
+        if(fleck_factor[i] < 0.8)
+            temp_diff *= fleck_factor[i];
         if(temp_diff > diff){
             which_one = 1;
             diff = temp_diff;
@@ -263,8 +263,6 @@ double MultigroupDiffusion::calculate_dt(double const dt,
                 diff = temp;
             }
         }
-		// if(fleck_factor[i] < 0.5)
-			// diff *= 0.1;
 		if(diff > max_diff)
 		{
             max_which = which_one;
@@ -292,7 +290,7 @@ double MultigroupDiffusion::calculate_dt(double const dt,
 	{
         // max_loc /= ENERGY_GROUPS_NUM;
 		std::cout<<"Radiation time step ID "<<cells[max_loc].ID<<" old Er "<<old_Er[max_loc]<<" new Er "<<cells[max_loc].Erad * cells[max_loc].density<<
-		" diff "<<max_diff<<" Tgas "<<cells[max_loc].temperature<<" Trad "<<std::pow(cells[max_loc].density * cells[max_loc].Erad * mass_scale_ / (length_scale_ * pow<2>(time_scale_) * CG::radiation_constant), 0.25)<<" max_Er "<<max_Er<<" rank "<<rank<<" density "<<cells[max_loc].density<<
+		" diff "<<max_diff<<" Tgas "<<cells[max_loc].temperature<<" Trad "<<std::pow(cells[max_loc].density * cells[max_loc].Erad * mass_scale_ / (length_scale_ * pow<2>(time_scale_) * CG::radiation_constant), 0.25)<<" max_Er "<<max_Er<<" max_rhoT "<<max_rhoT<<" rank "<<rank<<" density "<<cells[max_loc].density<<
 		" width "<<tess.GetWidth(max_loc)<<" Tgas_old "<<old_Tm[max_loc]<<" loc="<<tess.GetMeshPoint(max_loc)<<std::endl;
         std::cout<<"kp="<<sigma_absorption_planck[max_loc]<<" kr="<<sigma_absorption_average[max_loc]<<" fleck factor "<<fleck_factor[max_loc]<<" which one "<<max_which<<" equlibrium_factor "<<equlibrium_factor_final<<" final_Erad_eq "<<final_Erad_eq<<std::endl;
         if(max_which >= 2)
@@ -306,7 +304,7 @@ double MultigroupDiffusion::calculate_dt(double const dt,
 		// PrintDebugData(max_loc);
 	}
 
-    return std::min(dt * 0.15 / max_diff, dt*1.2);
+    return std::min(dt * 0.15 / max_diff, dt*1.4);
 }
 
 bool MultigroupDiffusion::step(double const tolerance, 
