ee8b1cae326771a4ce71555efae6116184f95254
ItamarShmelo <itamargiron@gmail.com>
2025-06-24 12:18:42 +0300

    Doppler test file
    Made changes to make the `doppler_test/test.cpp`.
    
    The test now consists of two adjacent cells in a cartesian grid, without hydro dynamics.
    Both cells are initialized with the same state expect the velocity which differ to create an artifical div(velocity).
    
    The spectrum is a trunckated Planckian.

--- runs/doppler_test/test.cpp
+++ runs/doppler_test/test.cpp
@@ -66,73 +66,71 @@ int main(void)
 	double const Emin = kev*1e-4;
 	double const Emax = kev*1e2;
 	
+	static_assert(ENERGY_GROUPS_NUM > 3, "Energy groups number must be greater than 3");
+
 	energy_groups_boundary[0] = Emin;
 	for(std::size_t g=0; g < G; ++g){
 		energy_groups_boundary[g+1] = std::pow(Emax/Emin, 1.0/G)*energy_groups_boundary[g];
 		energy_groups_center[g] = 0.5*(energy_groups_boundary[g+1]+energy_groups_boundary[g]);
 	}
 
-	double const dmin_eos = -50.656872045869001;
-	double const dmax_eos = 6.815166376138933;
-	double const dd_eos = 0.095310179804322;
+	// energy groups will be filled from energy_groups_boundary[thresh_hold_boundary_left] to energy_groups_boundary[thresh_hold_boundary_right]
+	// equivalent to filling only the energy groups `thresh_hold_boundary_left` up to `thresh_hold_boundary_right-1`
+	unsigned int const thresh_hold_boundary_left = 70;
+	unsigned int const thresh_hold_boundary_right = 85;	
+
+	double const E_thresh_left = energy_groups_boundary[thresh_hold_boundary_left];
+	double const E_thresh_right = energy_groups_boundary[thresh_hold_boundary_right];
+	
+	if(rank == 0) {
+		std::cout << "thresh_hold_boundary_left" << thresh_hold_boundary_left << ", thresh_hold_boundary_right: " << thresh_hold_boundary_right <<  std::endl;
+		std::cout << "E_thresh_left: " << E_thresh_left/kev << "KeV, E_thresh_right: " << E_thresh_right/kev << "KeV" << std::endl;
+	}
 
 	double const lscale = 1.;
 	double const mscale = 1.;
 	double const tscale = 1.;
-	// double const lscale = 1;
-	// double const mscale = 1;
-	// double const tscale = 1;
-	if (rank == 0)
-		std::cout << "start eos" << std::endl;
 
+	if (rank == 0) std::cout << "start eos" << std::endl;
+
+	// This eos is irrelevent to the test since there is no hydrodynamics
     double const cv = 1e15 / kev_kelvin;
     IdealGas eos(/*gamma=*/1.4, /*f=*/cv, /*beta=*/1.0, /*mu=*/0.0);
 
-	if (rank == 0)
-		std::cout << "end eos" << std::endl;
+	if (rank == 0) std::cout << "end eos" << std::endl;
 	
-    //Radiation
-    double const sigma_0_nom = 10.0*std::pow(kev, 3.5);
-
     using boost::math::pow;
-	AnalyticOpacity opacity(
-        [energy_groups_center, sigma_0_nom](ComputationalCell3D const& cell, std::size_t const group){ 
-            return CG::speed_of_light*std::sqrt(CG::boltzmann_constant*cell.temperature)*pow<3>(energy_groups_center[group])/(sigma_0_nom * 3.0);
-            },
-        [energy_groups_center, sigma_0_nom](ComputationalCell3D const& cell, std::size_t const group){ 
-            return sigma_0_nom/(std::sqrt(CG::boltzmann_constant*cell.temperature)*pow<3>(energy_groups_center[group]));
-            },
-        [](ComputationalCell3D const& cell, std::size_t group) { return 0.0; },
-
-        energy_groups_center,
-        energy_groups_boundary
-        );
+	ZeroAbsorptionZeroDiffusionMultigroup opacity{energy_groups_center, energy_groups_boundary};
 	
-    if (rank == 0)
-		std::cout << "end sta" << std::endl;
+    if (rank == 0) std::cout << "end sta" << std::endl;
 
-	const double width = 10 / lscale;
-	size_t const Nx = 128*4;
+	const double width = 10.0 / lscale;
+	size_t const Nx = 2;
 	Vector3D ll(0, -0.5 * width / Nx, -0.5 * width / Nx), ur(width, 0.5 * width / Nx, 0.5 * width / Nx);
 	Voronoi3D tess(ll, ur);
 
 	int counter = 0;
-	ComputationalCell3D init_cell;
-	double const T = ev_kelvin;
-	try
-	{
-		init_cell.density = 1. * lscale * lscale * lscale / mscale;
-		init_cell.temperature = T;
-		init_cell.internal_energy = eos.dT2e(init_cell.density, init_cell.temperature, init_cell.tracers, ComputationalCell3D::tracerNames);
-		init_cell.pressure = eos.de2p(init_cell.density, init_cell.internal_energy, init_cell.tracers, ComputationalCell3D::tracerNames);
-		init_cell.Erad = CG::radiation_constant * T * T * T * T * tscale * tscale / (init_cell.density * mscale / lscale);
-        for(std::size_t g=0; g < ENERGY_GROUPS_NUM; ++g){
-            init_cell.Eg[g] = planck_integral::planck_energy_density_group_integral(energy_groups_boundary[g], energy_groups_boundary[g+1], T);
-			init_cell.Eg[g] *= tscale * tscale / (init_cell.density * mscale / lscale); 
-			init_cell.Eg[g]  = std::max(init_cell.Eg[g], init_cell.Erad*1e-8);
+	
+	ComputationalCell3D init_cell_left;
+
+	double const T = kev_kelvin;
+
+	try {
+		init_cell_left.density = 1. * lscale * lscale * lscale / mscale;
+		init_cell_left.temperature = T;
+		init_cell_left.internal_energy = eos.dT2e(init_cell_left.density, init_cell_left.temperature, init_cell_left.tracers, ComputationalCell3D::tracerNames);
+		init_cell_left.pressure = eos.de2p(init_cell_left.density, init_cell_left.internal_energy, init_cell_left.tracers, ComputationalCell3D::tracerNames);
+		init_cell_left.velocity = Vector3D(0.0, 0.0, 0.0);
+
+		// fill only the energy groups between the thresholds
+		for(std::size_t g=thresh_hold_boundary_left; g < thresh_hold_boundary_right; ++g){
+            init_cell_left.Eg[g] = planck_integral::planck_energy_density_group_integral(energy_groups_boundary[g], energy_groups_boundary[g+1], T);
+			init_cell_left.Eg[g] *= tscale * tscale / (init_cell_left.density * mscale / lscale); 
         }
 
-		std::cout << "Erad=" << init_cell.Erad << ", sumEg=" << std::accumulate(init_cell.Eg.begin(), init_cell.Eg.end(), 0.0) << std::endl; 
+		init_cell_left.Erad = std::accumulate(init_cell_left.Eg.begin(), init_cell_left.Eg.end(), 0.0);
+
+		std::cout << "Erad=" << init_cell_left.Erad << std::endl;
 	}
 	catch (UniversalError const &eo)
 	{
@@ -140,16 +138,26 @@ int main(void)
 		throw;
 	}
 
+	ComputationalCell3D init_cell_right{init_cell_left};
+
+	// right cell has a velocity defined so there will be a velocity divergence
+	init_cell_right.velocity = Vector3D(1e9 * lscale / tscale, 0.0, 0.0);
 
 	vector<Vector3D> points; 
-	if(rank == 0)
-		points = CartesianMesh(Nx, 1, 1, ll, ur);
+	if(rank == 0) points = CartesianMesh(Nx, 1, 1, ll, ur);
+
 #ifdef RICH_MPI
 	tess.BuildParallel(points);
 #else
 	tess.Build(points);
 #endif
-	vector<ComputationalCell3D> cells(tess.GetPointNo(), init_cell);
+
+	size_t const Nlocal = tess.GetPointNo();
+	vector<ComputationalCell3D> cells(Nlocal);
+    for (size_t i = 0; i < Nlocal; ++i)
+    {
+		cells[i] = tess.GetMeshPoint(i).x < 0 ? init_cell_left : init_cell_right;
+    }
 
 	Hllc3D rs;
 	RigidWallGenerator3D ghost;
@@ -159,8 +167,15 @@ int main(void)
 	RoundCells3D pm(bpm, eos, 3.75, 0.01, false, 1.25);
 	
     double const Tb = kev_kelvin;
-	MultigroupDiffusionSideBoundary D_boundary(Tb, energy_groups_center, energy_groups_boundary);
-	MultigroupDiffusion matrix_builder(energy_groups_center, energy_groups_boundary, opacity, D_boundary, eos, std::vector<std::string> (), false, false, false, false);
+	MultigroupDiffusionClosedBoundary D_boundary{};
+
+	constexpr bool flux_limiter = false;
+	constexpr bool hydro_on = false;
+	constexpr bool compton_on = false;
+	constexpr bool doppler_on = true;
+	// constexpr bool doppler_on = false;
+
+	MultigroupDiffusion matrix_builder(energy_groups_center, energy_groups_boundary, opacity, D_boundary, eos, std::vector<std::string> (), flux_limiter, hydro_on, compton_on, doppler_on);
 	matrix_builder.length_scale_ = lscale;
 	matrix_builder.time_scale_ = tscale;
 	matrix_builder.mass_scale_ = mscale;
@@ -187,13 +202,14 @@ int main(void)
 	HDSim3D sim(tess, cells, eos, pm, tsf, fc, cu, eu, force, std::pair<std::vector<std::string>, std::vector<std::string>> (ComputationalCell3D::tracerNames, ComputationalCell3D::stickerNames), false, true);
 
 	double init_dt = 1e-13 / tscale;
-	double const tf = 1e-9 / tscale;
+	double const tf = 5e-8 / tscale;
 	double const dt_output = tf / 10.;
 	tsf.SetTimeStep(init_dt);
 	double nextT = dt_output;
 	double old_dt = init_dt;
 	vector<DiagnosticAppendix3D *> appendices;
-	WriteSnapshot3D(sim, "init.h5", appendices, true);
+	WriteSnapshot3D(sim, "snap_" + int2str(counter) + ".h5", appendices, true);
+	++counter;
 	while (sim.getTime() < tf)
 	{
 		if (sim.getCycle() % 1 == 0)
@@ -207,15 +223,15 @@ int main(void)
 		if (sim.getTime() > nextT)
 		{
 			WriteSnapshot3D(sim, "snap_" + int2str(counter) + ".h5", appendices, true);
-			nextT = sim.getTime() + dt_output;
 			++counter;
+			nextT = sim.getTime() + dt_output;
 		}
 		try
 		{
 			double new_dt = sim.RadiationTimeStep(old_dt, matrix_builder, true);
 			// tsf.SetTimeStep(new_dt);
 			// sim.SetTimeStep(new_dt);
-			new_dt=std::min(new_dt,1e-12);
+			new_dt=std::min(new_dt,tf/1000.0);
 			if (rank == 0)
 				std::cout<<"New time step is "<<new_dt<<std::endl;
 			old_dt = new_dt;
