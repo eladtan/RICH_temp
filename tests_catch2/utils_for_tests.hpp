4e7193e3b0c300b31441ccf7b8102f7d7e6ca94e
ItamarShmelo <itamargiron@gmail.com>
2025-11-25 15:43:11 +0200

    Made zip and unzip variadic

--- tests_catch2/utils_for_tests.hpp
+++ tests_catch2/utils_for_tests.hpp
@@ -4,6 +4,13 @@
 #include <vector>
 #include <stdexcept>
 #include <sstream>
+#include <type_traits>
+#include <json/json.h>
+#include <filesystem>
+#include <memory>
+#include <tuple>
+#include <utility>
+#include <fstream>
 
 namespace utils_for_tests {
     
@@ -11,36 +18,81 @@ int get_mpi_rank();
 
 int get_mpi_world_size();
 
-template<typename T, typename U>
-std::vector<std::pair<T, U>> zip(std::vector<T> const& vec_1, std::vector<U> const& vec_2){
-    
-    if(vec_1.size() != vec_2.size()){
-        std::ostringstream oss{"zip: vector sizes are not equal, vec_1.size() = "};
-        oss << vec_1.size()
-            << ", vec_2.size() = " << vec_2.size();
-        throw std::runtime_error(oss.str());
+template<typename First, typename... Rest>
+std::size_t size_of_first_in_pack(First const& vec, Rest const& ...){
+    return vec.size();
+}
+
+template<typename... Sizeables>
+bool has_same_size(Sizeables const&... objs){
+    static_assert(sizeof...(Sizeables) > 0, "has_same_size requires at least one argument");
+
+    auto const size = size_of_first_in_pack(objs...);
+    return ((size == objs.size()) && ...);
+}
+
+template<typename... Ts>
+std::vector<std::tuple<Ts...>> zip(std::vector<Ts> const&... vectors){
+    static_assert(sizeof...(Ts) > 1, "zip requires at least 2 vectors");
+
+    if(not has_same_size(vectors...)){
+        throw std::runtime_error("zip: vector sizes are not equal");
     }
     
-    std::vector<std::pair<T, U>> result;
-    auto const size = vec_1.size();
+    std::vector<std::tuple<Ts...>> result;
+    
+    auto const size = size_of_first_in_pack(vectors...);
     result.reserve(size);
 
     for(std::size_t i=0; i < size; ++i){
-        result.push_back(std::pair(vec_1[i], vec_2[i]));
+        result.emplace_back(vectors[i] ...);
     }
 
     return result;
 }
 
-template<typename T, typename U>
-std::pair<std::vector<T>, std::vector<U>> unzip(std::vector<std::pair<T, U>> const& vec){
-    
-    std::vector<T> vec_1;
-    std::vector<U> vec_2;
+template<typename... Ts>
+void reserve_vector_pack(
+    std::size_t size, 
+    std::tuple<std::vector<Ts>...>& vectors
+){    
+    std::apply(
+        [size](auto&... vecs){
+            (vecs.reserve(size), ...);
+        },
+        vectors
+    );
+}
 
+template <typename... Ts, std::size_t... Is>
+void push_back_tuple(
+    std::tuple<std::vector<Ts>...>& tuple_of_vectors,
+    std::tuple<Ts...> const& tuple,
+    std::index_sequence<Is...>
+) {
+    // expands to:
+    // get<0>(tuple_of_vectors).push_back(get<0>(tuple));
+    // get<1>(tuple_of_vectors).push_back(get<1>(tuple)); ...
+    (std::get<Is>(tuple_of_vectors).push_back(std::get<Is>(tuple)), ...);
+}
+
+template<typename... Ts>
+std::tuple<std::vector<Ts>...> unzip(std::vector<std::tuple<Ts...>> const& vec){
+    
+    std::tuple<std::vector<Ts>...> result{
+        std::vector<Ts>{}...
+    };
+    
     auto const size = vec.size();
-    vec_1.reserve(size);
-    vec_2.reserve(size);
+    reserve_vector_pack(size, result);
+    for(auto const& tup : vec){
+        push_back_tuple(result, tup, std::index_sequence_for<Ts...>{});
+    }
+    
+    return result;
+}
+
+namespace json {
 
     for(auto const& [elem_T, elem_U] : vec){
         vec_1.push_back(elem_T);
