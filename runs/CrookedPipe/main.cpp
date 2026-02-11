47b3e48f1e03e11b6acb2783c25be3eb51a8195a
Maor Miz <maorm2605@gmail.com>
2026-02-11 16:01:28 +0200

    Updated the CrookedPipe test

--- runs/CrookedPipe/main.cpp
+++ runs/CrookedPipe/main.cpp
@@ -1,30 +1,41 @@
 #include <mpi.h>
 #include <random>
+#include <filesystem>
 #include "mpi/mpi_commands.hpp"
-#include "3D/tesselation/voronoi/Voronoi3D.hpp"
+#include "3D/tessellation/voronoi/Voronoi3D.hpp"
 #include "misc/mesh_generator3D.hpp"
 #include "Radiation/CMMC/src/units/units.hpp"
 #include "newtonian/common/MixedEos.hpp"
 #include "newtonian/common/ideal_gas.hpp"
 #include "newtonian/three_dimensional/computational_cell.hpp"
 #include "newtonian/three_dimensional/conserved_3d.hpp"
-#include "monte/manager/MonteCarloManager.hpp"
-#include "monte/population/Comb.hpp"
-#include "monte/Voronoi3DMovement.hpp"
+#include "3D/monte/MonteCarloManager3D.hpp"
+#include "3D/monte/Voronoi3DMovement.hpp"
 #include "3D/output/write3D.hpp"
 #include "3D/radiation/RadiationIMC.hpp"
 #include "3D/radiation/PowerLawOpacity.hpp"
+#include "monte/population/Comb.hpp"
 #include "monte/boundary/SideTemperature.hpp"
 #include "3D/GeometryCommon/RoundGrid3D.hpp"
-#include "monte/Voronoi3DMovement.hpp"
 #include "CrookedPipeBoundary.hpp"
 #include "CrookedPipeOpacity.hpp"
+#include "3D/tessellation/voronoi/pointsManager/ParMETISPointManager.hpp"
+
+// #define RDMA
+
+namespace fs = std::filesystem;
 
 std::vector<ComputationalCell3D> *cellsPtr;
 Voronoi3D *tessPtr;
+bool do_output;
+
 
 void Output(const std::string &fname)
 {
+    if(not do_output)
+    {
+        return;
+    }
     rank_t rank;
     MPI_Comm_rank(MPI_COMM_WORLD, &rank);
     if(rank == 0)
@@ -44,7 +55,7 @@ void Output(const std::string &fname)
         tracers1[i] = cells[i].tracers[1];
     }
 
-    WriteVoronoiParallel(*tessPtr, fname, {temperatures, tracers0, tracers1}, {"Temperature", "Tracer0", "Tracer1"});
+    WriteVoronoiVTKOnly(*tessPtr, fname, {temperatures, tracers0, tracers1}, {"Temperature", "Tracer0", "Tracer1"});
 }
 
 vector<Vector3D> RandCylindar(std::size_t PointNum, double Rin, double Rout, double Zmin, double Zmax)
@@ -99,23 +110,28 @@ std::vector<Vector3D> GeneratePoints(size_t N, const Vector3D &ll, const Vector3
 
 int main(int argc, char *argv[])
 {
-    // vtune_stop();
+    vtune_stop();
+    DISABLE_TIMERS();
     
     MPI_Init(&argc, &argv);
+    MPI_Comm_set_errhandler(MPI_COMM_WORLD, MPI_ERRORS_ARE_FATAL);
+
     rank_t rank, size;
     MPI_Comm_rank(MPI_COMM_WORLD, &rank);
     MPI_Comm_size(MPI_COMM_WORLD, &size);
     if(rank == 0)
     {
-        if(argc != 3)
+        if(argc != 3 and argc != 4 and argc != 5)
         {
-            std::cerr << "Usage: " << argv[0] << " <number of points> <particles per cell>" << std::endl;
+            std::cerr << "Usage: " << argv[0] << " <number of points> <particles per cell> [output? = 1] [RDMA/P2P]" << std::endl;
             MPI_Abort(MPI_COMM_WORLD, 1);
         }
     }
 
     size_t N = std::stoul(argv[1]);
     size_t particlesPerCell = std::stoul(argv[2]);
+    do_output = (argc >= 4) ? std::stoul(argv[3]) : false;
+    bool useRDMA = (argc >= 5) ? (std::string(argv[4]) == "RDMA") : false;
 
     Vector3D ll(0, -2, -2), ur(7, 2, 2);
 
@@ -132,14 +148,34 @@ int main(int argc, char *argv[])
     {
         std::cout << "After spreading" << std::endl;
     }
-    points = RoundGrid3D(points, ll, ur, 5);
-    
-    Voronoi3D tess(ll, ur);
-    tessPtr = &tess;
-    tess.BuildParallel(points);
 
-    points = tess.getMeshPoints();
-    points.resize(tess.GetPointNo());
+    SILENCE_TIMERS();
+    points = RoundGrid3D(points, ll, ur, 5);
+    UNSILENCE_TIMERS();
+
+    Voronoi3D tess(ll, ur);
+
+    try
+    {
+        tessPtr = &tess;
+        {
+            START_TIMER("First Build");
+            tess.BuildParallel(points);
+            points = tess.getMeshPoints();
+            points.resize(tess.GetPointNo());
+        }
+        // std::shared_ptr<PointsManager> pointsManager = std::make_shared<ParMETISPointManager>(tess);
+        // tess.SetPointsManager(pointsManager);
+        tess.BuildParallel(points);
+        points = tess.getMeshPoints();
+        points.resize(tess.GetPointNo());
+    }
+    catch(const UniversalError &e)
+    {
+        reportError(e);
+        MPI_Abort(MPI_COMM_WORLD, 1);
+    }
+
     
     IdealGas eos1(1.5, 1e16 / units::kev_kelvin, 1, 0);
     IdealGas eos2(1.5, 1e13 / units::kev_kelvin, 1, 0);
@@ -227,14 +263,32 @@ int main(int argc, char *argv[])
     std::shared_ptr<RadiationIMC> physics = std::make_shared<RadiationIMC>(tess, boundaryCond, cells, conserved, eos, opacity, particlesPerCell / 10 /* 100 */);
     std::shared_ptr<PopulationControl<Vector3D, Tessellation3D>> popControl = std::make_shared<CombPopulationControl<Vector3D, Tessellation3D>>(tess, particlesPerCell /* 100 */);
 
-    Output("CrookedPIPE_start.vtu");
+    std::string prefix = "/data/shared/maorm/OSC_CrookedPIPE_" + std::to_string(size) + "_";
+    fs::path path = fs::path(prefix);
+
+    // create prefix (directory)
+    if(not fs::exists(path))
+    {
+        fs::create_directories(path);
+    }
+
+    Output(prefix + "start.vtu");
 
     {
-        MonteCarloManager<Vector3D, Tessellation3D> manager(tess, physics, popControl, boundaryCond);
-        
+        std::shared_ptr<MonteCarloManager3D> manager;
+        if(useRDMA)
+        {
+            manager = std::make_shared<RDMAMonteCarloManager3D>(tess, physics, popControl, boundaryCond);
+        }
+        else
+        {
+            manager = std::make_shared<TwoSidedMonteCarloManager3D>(tess, physics, popControl, boundaryCond);
+        }
+
         std::vector<MonteCarloParticle<Vector3D, Tessellation3D>> particles;
         size_t iterations = 10000;
         std::chrono::high_resolution_clock::time_point start, end1, end2;
+        std::chrono::high_resolution_clock::time_point start_total, end_total;
 
         // vtune_start();
         double dt = 1e-11; 
@@ -244,16 +298,22 @@ int main(int argc, char *argv[])
 
         std::vector<double> weights(points.size(), 1.0);
 
-        double max_step = 5e-10;// 1e-8
+        double max_step = 1e-9;// 1e-8
 
-        // while(time < totalTime)
-        for(int k = 0; k < 60; k++)
+        start_total = std::chrono::high_resolution_clock::now();
+        
+        while(time < totalTime)
+        // for(int k = 0; k < 21; k++)
         {
             start = std::chrono::high_resolution_clock::now();
-
+            // if(k == 50)
+            // {
+            //     vtune_start();
+            // }
             try
             {
-                particles = manager.step(particles, dt);
+                START_TIMER("MC Step");
+                particles = manager->step(particles, cells, dt);
             }
             catch(const UniversalError &e)
             {
@@ -268,61 +328,96 @@ int main(int argc, char *argv[])
                 double timeTaken = std::chrono::duration<double>(end1 - start).count();
                 std::cout << "Ended Iteration " << i << ", dt: " << dt << ", simulation time: " << time << " (" << time / totalTime * 100 << "%), time taken: " << timeTaken << " seconds" << std::endl;
             }
-            if(i % 10 == 0)
+            if(i % 5 == 0)
             {
-                Output("CrookedPIPE_" + std::to_string(i) + ".vtu");
+                Output(prefix + std::to_string(i) + ".vtu");
             }
             i++;
             
             MPI_Barrier(MPI_COMM_WORLD);
+            // vtune_stop();
 
             start = std::chrono::high_resolution_clock::now();
-
-            try
+            if(i % 10 == 0)
             {
-                if(i % 10 == 0)
+                vtune_start();
+                try
                 {
-                    const std::vector<double> &factorFlecks = physics->getFactorFleck();
-                    const std::vector<double> &planckOpacities = physics->getPlanckOpacities();
-                    size_t N = tess.GetPointNo();
-                    weights.resize(N, 1.0);
-                    for(size_t j = 0; j < N; j++)
+                    START_TIMER("Rebalance & Exchange");
                     {
-                        weights[j] = std::max(1., planckOpacities[j] * tess.GetWidth(j) * (1 - factorFlecks[j]));
+                        START_TIMER("Rebalance");
+                        SILENCE_TIMERS();
+                        const std::vector<double> &factorFlecks = physics->getFactorFleck();
+                        const std::vector<double> &planckOpacities = physics->getPlanckOpacities();
+                        size_t N = tess.GetPointNo();
+                        // weights.resize(N, 1.0);
+                        // double weight = static_cast<double>(manager.GetStepCounter()) / N;
+                        // for(size_t j = 0; j < N; j++)
+                        // {
+                        //     // weights[j] = std::max(1., planckOpacities[j] * tess.GetWidth(j) * (1 - factorFlecks[j]));
+                        //     weights[j] = weight;
+                        // }
+                        weights.resize(N);
+                        const std::vector<size_t> &counters = manager->GetCellsStepsCounters();
+                        for(size_t j = 0; j < N; j++)
+                        {
+                            weights[j] = static_cast<double>(counters[j]);
+                        }
+                        // std::cout << "Rank " << rank << ", weights " << weights << std::endl;
+                        tess.BuildParallel(points, weights);
+                        points = tess.getMeshPoints();
+                        points.resize(tess.GetPointNo());
+                        
+                        MPI_exchange_data(tess, cells, false);
+                        MPI_exchange_data(tess, conserved, false);
+                        
+                        end1 = std::chrono::high_resolution_clock::now();
+        
+                        UNSILENCE_TIMERS();
                     }
-                    tess.BuildParallel(points, weights);
-                    points = tess.getMeshPoints();
-                    points.resize(tess.GetPointNo());
-                    
-                    MPI_exchange_data(tess, cells, false);
-                    MPI_exchange_data(tess, conserved, false);
-                    
-                    end1 = std::chrono::high_resolution_clock::now();
-    
-                    UpdateNewCells(tess, particles);
+                    {
+                        START_TIMER("Exchange");
+                        if(rank == 0)
+                        {
+                            std::cout << "Calling UpdateNewCellsAfterExchange" << std::endl;
+                        }
+                        UpdateNewCells(tess, particles, cells);
+                        // UpdateNewCellsAfterExchange(tess, particles);
+                        // std::cout << "Rank " << rank << " has now " << particles.size() << " particles after exchange." << std::endl;
+                    }
     
                     end2 = std::chrono::high_resolution_clock::now();
-                    if(rank == 0 /* and i % 100 == 0 */)
-                    {
-                        double timeTaken = std::chrono::duration<double>(end2 - start).count();
-                        double timeTakenOnlyRebalance = std::chrono::duration<double>(end1 - start).count();
-                        std::cout << "Did rebalance, time taken: " << timeTaken << " seconds, only rebalance (no exchange): " << timeTakenOnlyRebalance << std::endl;
-                    }
+                    MPI_Barrier(MPI_COMM_WORLD);
+        
+                    vtune_start();
                 }
+                catch(const UniversalError &e)
+                {
+                    reportError(e);
+                    exit(1);
+                }
+                vtune_stop();
 
+                if(rank == 0 /* and i % 100 == 0 */)
+                {
+                    PRINT_TIMES();
+                    double timeTaken = std::chrono::duration<double>(end2 - start).count();
+                    double timeTakenOnlyRebalance = std::chrono::duration<double>(end1 - start).count();
+                    std::cout << "Did rebalance, time taken: " << timeTaken << " seconds, only rebalance (no exchange): " << timeTakenOnlyRebalance << std::endl;
+                }
+                CLEAR_TIMES();
             }
-            catch(const UniversalError &e)
-            {
-                reportError(e);
-                exit(1);
-            }
-            MPI_Barrier(MPI_COMM_WORLD);
         }
 
-        // vtune_stop();
+        end_total = std::chrono::high_resolution_clock::now();
+        double totalTimeTaken = std::chrono::duration<double>(end_total - start_total).count();
+        if(rank == 0)
+        {
+            std::cout << "Total time taken: " << totalTimeTaken << " seconds" << std::endl;
+        }
     }
 
-    Output("CrookedPIPE_final.vtu");
+    Output(prefix + "final.vtu");
 
     MPI_Finalize();
 }
