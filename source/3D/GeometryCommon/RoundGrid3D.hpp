9e8ba79c7a58dfea9bc0ef4424fdae668d126125
eladtan <eladtan1@gmail.com>
2026-03-08 08:43:09 +0200

    added verbose and pressure avg term to make volumes uniform

--- source/3D/GeometryCommon/RoundGrid3D.hpp
+++ source/3D/GeometryCommon/RoundGrid3D.hpp
@@ -43,8 +43,9 @@ vector<Vector3D> RoundGrid3DSingle(vector<Vector3D> const& points, Vector3D cons
     \param NumberIt The number of iterations for the correction process. Defaults to 10.
     \param tess An optional pointer to a Tessellation3D object for additional tessellation data. Defaults to nullptr.
     \param criteria A function to determine if a point should be included in the adjustment process. Defaults to a function that returns true for all points.
+    \param verbose Whether to print iteration progress to stdout. Defaults to true.
     \return A vector of Vector3D objects representing the adjusted points that result in a rounder tessellation.
 */
 std::vector<Vector3D> RoundGridSphere3D(std::vector<Vector3D> const& points, Vector3D const& ll, Vector3D const& ur, Vector3D const center = Vector3D(0,0,0),
-    size_t NumberIt = 10,	Tessellation3D *tess = nullptr, std::function<bool(Vector3D)> const& criteria = [](Vector3D){return true;});
+    size_t NumberIt = 10,	Tessellation3D *tess = nullptr, std::function<bool(Vector3D)> const& criteria = [](Vector3D){return true;}, bool verbose = true);
 #endif //ROUNDGRID3D
