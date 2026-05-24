a861a5d39d4a6609f6601ec539e839e6e84edb7b
eladtan <eladtan1@gmail.com>
2026-05-24 09:17:09 +0300

    feat(radiation): Update opacity calculations to ensure minimum temperature threshold
    Modify the opacity calculation methods in MCPowerLawOpacity to enforce a minimum temperature threshold of 1.0. This change improves the accuracy of opacity computations by preventing negative temperature values from affecting the results, enhancing the robustness of radiation simulations.

--- source/3D/radiation/PowerLawOpacity.cpp
+++ source/3D/radiation/PowerLawOpacity.cpp
@@ -8,12 +8,14 @@ MCPowerLawOpacity::MCPowerLawOpacity(double sigmaA0, double sigmaS0, double sigm
 
 double MCPowerLawOpacity::CalcPlanckOpacity(const ComputationalCell3D &cell) const
 {
-    return this->sigmaA0 * std::pow(cell.density, this->sigmaA_rho) * std::pow(cell.temperature, /*std::min(cell.temperature, 10 * units::kev_kelvin),*/ this->sigmaA_T);
+    double const T = std::max(cell.temperature, 1.0);
+    return this->sigmaA0 * std::pow(cell.density, this->sigmaA_rho) * std::pow(T, this->sigmaA_T);
 }
 
 double MCPowerLawOpacity::CalcScatteringOpacity(const ComputationalCell3D &cell) const
 {
-    return this->sigmaS0 * std::pow(cell.density, this->sigmaS_rho) * std::pow(cell.temperature, this->sigmaS_T);
+    double const T = std::max(cell.temperature, 1.0);
+    return this->sigmaS0 * std::pow(cell.density, this->sigmaS_rho) * std::pow(T, this->sigmaS_T);
 }
 
 double MCPowerLawOpacity::CalcAbsorptionOpacity(const ComputationalCell3D &cell, double energy) const
