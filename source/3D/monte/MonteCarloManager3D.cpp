c492750a8c8d703efc42c312286141831c25eb5b
eladtan <eladtan1@gmail.com>
2026-05-24 09:09:51 +0300

    feat(montecarlo): Integrate radiation physics into Monte Carlo manager
    Enhance the RDMAMonteCarloManager3D class by incorporating radiation physics functionality. A dynamic cast is added to retrieve opacity from the MonteCarloRadiationPhysics3D instance, and a pointer to the computational cells is established for improved particle processing. This update aims to extend the capabilities of the Monte Carlo simulation framework in three-dimensional space.

--- source/3D/monte/MonteCarloManager3D.cpp
+++ source/3D/monte/MonteCarloManager3D.cpp
@@ -1,5 +1,6 @@
 #include "MonteCarloManager3D.hpp"
 #include "newtonian/three_dimensional/computational_cell.hpp"
+#include "3D/radiation/MonteCarloPhysics3D.hpp"
 
 MonteCarloManagerSerial3D::MonteCarloManagerSerial3D(const Tessellation3D &grid, const std::shared_ptr<MonteCarloPhysics<Vector3D, Tessellation3D>> &physics,
     const std::shared_ptr<PopulationControl<Vector3D, Tessellation3D>> &populationControl,
@@ -25,10 +26,15 @@ RDMAMonteCarloManager3D::RDMAMonteCarloManager3D(const Tessellation3D &grid, con
                                          const std::shared_ptr<PopulationControl<Vector3D, Tessellation3D>> &populationControl,
                                          const std::shared_ptr<BoundaryCondition<Vector3D, Tessellation3D>> &boundaryCondition,
                                          size_t bufferSizes, const MPI_Comm &comm, RDMA_Type rdma_type): MonteCarloManager<Vector3D, Tessellation3D>(grid, physics, populationControl, boundaryCondition, bufferSizes, comm, rdma_type)
-{}
+{
+    auto* radPhysics = dynamic_cast<MonteCarloRadiationPhysics3D*>(physics.get());
+    if (radPhysics)
+        this->progressOpacityPtr_ = radPhysics->getOpacity();
+}
 
 std::vector<typename RDMAMonteCarloManager3D::MCParticle> RDMAMonteCarloManager3D::step(std::vector<MCParticle> &&particleList, const std::vector<ComputationalCell3D> &cells, dt_t fullDt)
 {
+    this->progressCellsPtr_ = &cells;
     std::vector<MCParticle> newParticles = MonteCarloManager<Vector3D, Tessellation3D>::step(std::move(particleList), fullDt);
     for(MCParticle &p : newParticles)
     {
