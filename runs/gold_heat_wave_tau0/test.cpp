3e7060895b06014177a2963997e325f834705fb1
eladtan <eladtan1@gmail.com>
2026-05-27 16:27:54 +0300

    feat(test): Enhance test.cpp with new boundary conditions and diagnostics
    Add functions to find the leftmost and rightmost cells in the tessellation, and implement a new boundary condition class for moving Marshak scenarios. Update the main simulation loop to set face velocities based on these cells and write surface diagnostics to a file, improving the simulation's capability to analyze boundary interactions and temperature dynamics.

--- runs/gold_heat_wave_tau0/test.cpp
+++ runs/gold_heat_wave_tau0/test.cpp
@@ -33,12 +33,42 @@
 #include <cmath>
 #include <numeric>
 #include <algorithm>
+#include <limits>
 #include <fenv.h>
 #include <libgen.h>
 #include <string.h>
 #ifdef RICH_MPI
 #include <mpi.h>
 #endif
+
+size_t find_leftmost_cell(Tessellation3D const& tess)
+{
+	size_t left_i = 0;
+	double xmin = std::numeric_limits<double>::max();
+	for (size_t i = 0; i < tess.GetPointNo(); ++i)
+	{
+		double const x = tess.GetMeshPoint(i).x;
+		if (x < xmin) { xmin = x; left_i = i; }
+	}
+	return left_i;
+}
+
+size_t find_rightmost_cell(Tessellation3D const& tess)
+{
+	size_t right_i = 0;
+	double xmax = -std::numeric_limits<double>::max();
+	for (size_t i = 0; i < tess.GetPointNo(); ++i)
+	{
+		double const x = tess.GetMeshPoint(i).x;
+		if (x > xmax) { xmax = x; right_i = i; }
+	}
+	return right_i;
+}
+
+double shock_speed_from_pressure(double const rho0, double const rho2, double const P2)
+{
+	return std::sqrt(P2 / std::max(rho0 * (1.0 - rho0 / rho2), 1e-300));
+}
 
 class XOnlyMotion3D : public PointMotion3D
 {
@@ -88,7 +118,7 @@ int main(int argc, char* argv[])
 	// D = c / (3*sigma_R) = [c / (3*7200)] * HeV_K^{1.5} * T^{1.5} * rho^{-1.2}
 	double const D0 = CG::speed_of_light / (3.0 * 7200.0 * std::pow(HeV_K, 1.5));
 	// sigma_P = sigma_R = 7200 * HeV_K^{1.5} * rho^{1.2} * T^{-1.5}
-	double const planck0 = 7200.0 * std::pow(HeV_K, 1.5);
+	double const planck0 = 100*7200.0 * std::pow(HeV_K, 1.5);
 	PowerLawOpacity opacity(D0, -1.2, 1.5, planck0, 1.2, -1.5);
 
 	// Domain: 1D slab along x
@@ -170,10 +200,15 @@ int main(int argc, char* argv[])
 	RoundCells3D round_cells(lagrangian, eos, 0.1);
 	XOnlyMotion3D pm(round_cells);
 
-	// Radiation boundary: blackbody on left x-face, zero flux elsewhere
 	double const t_start = 1e-14 * 512 / Nx;
+
+	// --- Boundary choice ---
+	// Test A (Dirichlet diagnostic): DiffusionDirichletBoundary D_boundary(HeV_K, opacity);
+	// Test B (old Marshak):          DiffusionSideBoundary D_boundary(T_bath_init);
+	// Test C (moving Marshak):
 	double const T_bath_init = std::pow(1.0 + 0.169141 * std::pow(t_start / 1e-9, -79.0 / 192.0), 0.25) * HeV_K;
-	DiffusionSideBoundary D_boundary(T_bath_init);
+	DiffusionMovingMarshakBoundary D_boundary(T_bath_init);
+	D_boundary.SetDebug(false);
 
 	// Diffusion: no flux limiter, hydro on, no compton
 	Diffusion diffusion(opacity, D_boundary, eos, std::vector<std::string>(),
@@ -300,20 +335,59 @@ int main(int argc, char* argv[])
 	double const tf = 2e-9;
 	double old_dt = 1e-16;
 	simulation.SetTime(t_start);
+
+	std::string diag_path = dir_path + "/boundary_diagnostics.txt";
+	std::ofstream diag_out;
+	if (rank == 0)
+	{
+		diag_out.open(diag_path);
+		diag_out << "# time T_bath_HeV T_surface_mat_HeV T_surface_rad_HeV rho_surface P_surface v_surface v_face_left x_left_box x_left_cell\n";
+	}
+
 	while (simulation.GetTime() < tf)
 	{
-		double const t_now = simulation.GetTime();
-		double const t_ns = (t_now + old_dt * 0.5) / 1e-9;
-		double const T_bath = std::pow(1.0 + 0.169141 * std::pow(t_ns, -79.0 / 192.0), 0.25) * HeV_K;
+		double const dt = std::min(old_dt, tf - simulation.GetTime());
+
+		// Update bath temperature at midpoint of timestep
+		double const t_mid_ns = (simulation.GetTime() + 0.5 * dt) / 1e-9;
+		double const T_bath = std::pow(1.0 + 0.169141 * std::pow(t_mid_ns, -79.0 / 192.0), 0.25) * HeV_K;
 		D_boundary.SetTemperature(T_bath);
+
+		// Set face velocities from globally leftmost/rightmost cells
+		{
+			auto const& cur_tess = sim.getTessellation();
+			auto const& cur_cells = sim.getCells();
+			size_t const left_i = find_leftmost_cell(cur_tess);
+			size_t const right_i = find_rightmost_cell(cur_tess);
+
+			double left_x = cur_tess.GetMeshPoint(left_i).x;
+			double right_x = cur_tess.GetMeshPoint(right_i).x;
+			Vector3D left_vel = cur_cells[left_i].velocity;
+			Vector3D right_vel = cur_cells[right_i].velocity;
+
+#ifdef RICH_MPI
+			struct { double x; int rank; } left_data, right_data;
+			left_data.x = left_x;
+			left_data.rank = rank;
+			right_data.x = right_x;
+			right_data.rank = rank;
+
+			MPI_Allreduce(MPI_IN_PLACE, &left_data, 1, MPI_DOUBLE_INT, MPI_MINLOC, MPI_COMM_WORLD);
+			MPI_Allreduce(MPI_IN_PLACE, &right_data, 1, MPI_DOUBLE_INT, MPI_MAXLOC, MPI_COMM_WORLD);
+
+			MPI_Bcast(&left_vel, 3, MPI_DOUBLE, left_data.rank, MPI_COMM_WORLD);
+			MPI_Bcast(&right_vel, 3, MPI_DOUBLE, right_data.rank, MPI_COMM_WORLD);
+#endif
+			D_boundary.SetLeftFaceVelocity(left_vel);
+			D_boundary.SetRightFaceVelocity(right_vel);
+		}
 
 		try
 		{
-			simulation.SetTimeStep(old_dt);
+			simulation.SetTimeStep(dt);
 			simulation.step();
 			double new_dt = radStep->suggestTimeStep();
 			new_dt = std::min(new_dt, tsf->GetTimeStep());
-			// new_dt = std::min(std::max(1e-14, simulation.GetTime() * 1e-3), new_dt);
 			new_dt = std::max(1e-16 * 512 / Nx, new_dt);
 			old_dt = new_dt;
 		}
@@ -326,6 +400,47 @@ int main(int argc, char* argv[])
 		if (rank == 0)
 			std::cout << "Cycle " << simulation.GetCycle() << " Time " << simulation.GetTime()
 				<< " dt " << old_dt << std::endl;
+
+		// Write surface diagnostics (gather global leftmost cell info)
+		if (simulation.GetCycle() % 50 == 0)
+		{
+			auto const& cur_tess = sim.getTessellation();
+			auto const& cur_cells = sim.getCells();
+			size_t const left_i = find_leftmost_cell(cur_tess);
+
+			struct { double T_mat, T_rad, rho, P, vx, x_cell; } surf;
+			surf.x_cell = cur_tess.GetMeshPoint(left_i).x;
+			ComputationalCell3D const& c = cur_cells[left_i];
+			surf.T_mat = c.temperature;
+			double const Er_vol = c.Erad * c.density;
+			surf.T_rad = std::pow(std::max(Er_vol / CG::radiation_constant, 1e-300), 0.25);
+			surf.rho = c.density;
+			surf.P = c.pressure;
+			surf.vx = c.velocity.x;
+
+#ifdef RICH_MPI
+			struct { double x; int rank; } loc_data;
+			loc_data.x = surf.x_cell;
+			loc_data.rank = rank;
+			MPI_Allreduce(MPI_IN_PLACE, &loc_data, 1, MPI_DOUBLE_INT, MPI_MINLOC, MPI_COMM_WORLD);
+			MPI_Bcast(&surf, 6, MPI_DOUBLE, loc_data.rank, MPI_COMM_WORLD);
+#endif
+
+			if (rank == 0)
+			{
+				diag_out << std::scientific << std::setprecision(12)
+					<< simulation.GetTime() << " "
+					<< T_bath / HeV_K << " "
+					<< surf.T_mat / HeV_K << " "
+					<< surf.T_rad / HeV_K << " "
+					<< surf.rho << " "
+					<< surf.P << " "
+					<< surf.vx << " "
+					<< surf.vx << " "
+					<< cur_tess.GetBoxCoordinates().first.x << " "
+					<< surf.x_cell << "\n";
+			}
+		}
 
 		if (simulation.GetCycle() % 200 == 0)
 			write_output();
